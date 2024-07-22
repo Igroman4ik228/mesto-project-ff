30425cf7053be1799fd65987d7fc3499e5984468
Igroman4ik <ni4000009@gmail.com>
2024-07-22 18:45:42 +0300

    done

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(cardData, deleteCardFunc) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  cardElement.querySelector('.card__image').src = cardData.link;
 
-// @todo: Функция удаления карточки
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  cardDeleteButton.addEventListener('click', () => deleteCardFunc(cardElement))
 
-// @todo: Вывести карточки на страницу
+  return cardElement; // Возврат готовой карточки
+}
+
+// Функция удаления карточки
+const deleteCard = (cardElement) => cardElement.remove();
+
+// Вывести карточки на страницу
+initialCards.forEach((initialCard) => {
+  const resultCard = createCard(initialCard, deleteCard)
+  placesList.append(resultCard);
+});
